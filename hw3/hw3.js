88ec1b3653ca20c08ed58eab5b445ea5bbc449cf
Chun-Ching-Hsu <68492756+Chun-Ching-Hsu@users.noreply.github.com>
2022-04-01 10:59:20 +0800

    second-modi

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -2,23 +2,24 @@ var s ="";
 var s_time=0;
 var num=0;
 var tmp = "qazwsxedcrfvtgbyhnujmikolp"; 
+var s_len =0;
 document.addEventListener('keydown',getchar);
 function getchar(lokey){
     creat_s();
-    //console.log(s[s.length-1],lokey.key);
     if(s[s.length-1] == lokey.key){
         s = s.substring(0,s.length-1);
     }
     document.getElementById("hw3").innerHTML = s ;
 }
 function creat_s(){
-    num = get_t()*2.5;
-    if(s.length<num) { 
-        for (var i=0 ;i< num-s.length ; i++){
-            var rand = Math.floor(Math.random()*26);
-            s = tmp[rand]+s;
-        }
+    num = get_t()*2.5; 
+    for (var i=0 ;i< num-s_len ; i++){
+        var rand = Math.floor(Math.random()*26);
+        s = tmp[rand]+s;
     }
+    s_len = num;
+    console.log(s_len);
+    console.log(num);
 }
 function start_t(){
     s_time = new Date().getTime();
