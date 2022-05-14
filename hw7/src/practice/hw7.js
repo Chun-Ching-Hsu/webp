cd7149dbb03fb6b2e2f1ff567ad20bcac149266c
Chun-Ching-Hsu <68492756+Chun-Ching-Hsu@users.noreply.github.com>
2022-05-14 11:51:59 +0800

    new

--- hw7/src/practice/hw7.js
+++ hw7/src/practice/hw7.js
@@ -3,12 +3,12 @@ import './hw7.css'
 function LogIn(){
     return(
         <div className="login">
-            <h1 className='head'>CGU Login</h1>
+            <h1>CGU Login</h1>
             <form>
-                <label id="User">User Name </label>
-                <input id = "User-in" type='text' required/><br/>
-                <label id="PW">Password</label>
-                <input id="PW-in" type='password' required/><br/>
+                <label type="text">User Name </label><br/>
+                <input className='in' required/><br/><br/>
+                <label type="text">Password</label><br/>
+                <input className='in' type='password' required/><br/>
                 <button id="sigin">登入</button>
             </form>
         </div>
@@ -16,3 +16,15 @@ function LogIn(){
 }
 
 export default LogIn;
+// import './hw7.css';
+// function Login(){
+//        return(
+//             < div class='box'>
+//                 <h1>CGU Login</h1> 
+//                <h2>User name<input type='text' class='text'></input></h2>      
+//                <h2>Password<input type='text'class='text'></input></h2> 
+//                <div><button id="button" >登入</button></div>   
+//             </div >
+//        )       
+// }
+// export default Login;
