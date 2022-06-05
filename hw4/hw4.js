4ab912e16c26045a1ee302e54a93d70d4ed63ec2
Chun-Ching-Hsu <68492756+Chun-Ching-Hsu@users.noreply.github.com>
2022-06-06 02:20:02 +0800

    new change

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
     $("input").click(function(){ 
         s_time=new Date().getTime();
         creat_s();
-        document.addEventListener('keydown',creat_s);
+        window.setInterval(creat_s,1500);
+        //document.addEventListener('keydown',creat_s);
         document.addEventListener('keydown',delete_ch);
         window.setInterval(end,1000);
     });
@@ -18,33 +19,37 @@ function move(){
     $(".trail").find("img").animate({left:"97%"},20000);
 }
 function delete_ch(lokey){
+    console.log(lokey)
     let del_node = document.createElement("img");
     del_node.src = "alpha/"+(lokey.keyCode-65)+".png";
     for (let x =0 ; x<=5 ; x++){
-        let last = $(".trail")[x].children[0];
-        if (last.src == del_node.src){
-            $(".trail")[x].removeChild(last);
-            break;
+        if($(".trail")[x].children[0] != null){
+            let last = $(".trail")[x].children[0];
+            if (last.src == del_node.src){
+                $(".trail")[x].removeChild(last);
+                break;
+            }
         }
     }
     //console.log(last);
 }
 function creat_s(){ 
     let num=[] ;
-    let total_s = get_t()*1.5;
+    // let total_s = get_t()*1.5;
     let target = Math.floor(Math.random()*6)
-    for( let s=0 ; s<total_s-s ; s++){
+    console.log(target)
+    // for( let s=0 ; s<total_s-s ; s++){
         // for (let i=0 ; i<=5 ; i++){
-            num[target] = Math.floor(Math.random()*25); 
+    num[target] = Math.floor(Math.random()*25); 
         // }
         //for (let x = 0 ; x <=5 ; x++){
-            let newNode =  document.createElement("img"); 
-            newNode.src="alpha/"+num[target]+".png";
-            newNode.style.zIndex=count;
-            $(".trail")[target].append(newNode);
+    let newNode =  document.createElement("img"); 
+    newNode.src="alpha/"+num[target]+".png";
+    newNode.style.zIndex=count;
+    $(".trail")[target].append(newNode);
         //}
-        count--;
-    }
+    count--;
+    // }
     move();
 }
 
@@ -55,10 +60,13 @@ function get_t(){
 }
 function end(){
     for(var y = 0 ; y <= 5 ; y++){
-        var pos = $(".trail")[y].children[0];
-        if(pos.style.left >= "94%" ){
-            alert("Game over");
-            $('img').stop(true);
+        if ($(".trail")[y].children[0] != null){
+            //console.log($(".trail")[y].children[0])
+            var pos = $(".trail")[y].children[0];
+            if(pos.style.left >= "94%" ){
+                alert("Game over");
+                $('img').stop(true);
+            }
         }
     }
 }
